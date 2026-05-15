94e5620aa7f6ebd6647b0a59020d87f8948d18ea
nashsmartdom <90872280+nashsmartdom@users.noreply.github.com>
2026-05-15 23:47:29 +0200

    Add constants

---
+++ custom_components/room_remote_control/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "room_remote_control"
+CONF_REMOTES = "remotes"
+CONF_TOPICS = "topics"
+CONF_LAMPS = "lamps"
+CONF_EXTRA_OFF = "extra_off"
+CONF_ACTIONS = "actions"
+CONF_EFFECTS = "effects"
+CONF_BRIGHTNESS_CYCLE = "brightness_cycle"
+
+DEFAULT_BRIGHTNESS_CYCLE = [30, 10, 100, 80]
