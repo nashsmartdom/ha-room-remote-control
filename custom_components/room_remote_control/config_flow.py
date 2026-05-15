14934d281be030acebc6fe953527e8bd798e1935
nashsmartdom <90872280+nashsmartdom@users.noreply.github.com>
2026-05-16 00:46:48 +0200

    Remove manual effects from UI flow

--- custom_components/room_remote_control/config_flow.py
+++ custom_components/room_remote_control/config_flow.py
@@ -14,7 +14,6 @@ CONF_TOPICS_TEXT = "topics_text"
 CONF_LIGHTS = "lights"
 CONF_EXTRA_OFF = "extra_off"
 CONF_BUTTONS_TEXT = "buttons_text"
-CONF_EFFECTS_TEXT = "effects_text"
 
 DEFAULT_BUTTONS = """on=cycle:light.wiz_3
 arrow_left_click=cycle:light.wiz_7
@@ -30,10 +29,6 @@ down=all_off
 off_hold=all_off
 off=all_off
 arrow_right_hold=next_effect"""
-
-DEFAULT_EFFECTS = """Warm white|80|435
-Daylight|100|
-Candlelight|25|"""
 
 class RoomRemoteControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -52,7 +47,6 @@ class RoomRemoteControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_LIGHTS, default=["light.wiz_3", "light.wiz_7", "light.wiz_al_1", "light.wiz_4"]): selector.EntitySelector(selector.EntitySelectorConfig(domain="light", multiple=True)),
                 vol.Optional(CONF_EXTRA_OFF, default=["light.hochbett"]): selector.EntitySelector(selector.EntitySelectorConfig(domain="light", multiple=True)),
                 vol.Required(CONF_BUTTONS_TEXT, default=DEFAULT_BUTTONS): selector.TextSelector(selector.TextSelectorConfig(multiline=True)),
-                vol.Optional(CONF_EFFECTS_TEXT, default=DEFAULT_EFFECTS): selector.TextSelector(selector.TextSelectorConfig(multiline=True)),
             }
         )
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
@@ -76,7 +70,6 @@ class RoomRemoteControlOptionsFlow(config_entries.OptionsFlow):
                 vol.Required(CONF_LIGHTS, default=data.get(CONF_LIGHTS, [])): selector.EntitySelector(selector.EntitySelectorConfig(domain="light", multiple=True)),
                 vol.Optional(CONF_EXTRA_OFF, default=data.get(CONF_EXTRA_OFF, [])): selector.EntitySelector(selector.EntitySelectorConfig(domain="light", multiple=True)),
                 vol.Required(CONF_BUTTONS_TEXT, default=data.get(CONF_BUTTONS_TEXT, DEFAULT_BUTTONS)): selector.TextSelector(selector.TextSelectorConfig(multiline=True)),
-                vol.Optional(CONF_EFFECTS_TEXT, default=data.get(CONF_EFFECTS_TEXT, DEFAULT_EFFECTS)): selector.TextSelector(selector.TextSelectorConfig(multiline=True)),
             }
         )
         return self.async_show_form(step_id="init", data_schema=schema)
