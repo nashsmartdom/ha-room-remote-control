021bb2354311c23be41f77673ea6bc51e1241523
nashsmartdom <90872280+nashsmartdom@users.noreply.github.com>
2026-05-16 02:52:32 +0200

    Use stable config flow without device selector

--- custom_components/room_remote_control/config_flow.py
+++ custom_components/room_remote_control/config_flow.py
@@ -10,7 +10,6 @@ from homeassistant.helpers import selector
 
 from .const import DOMAIN
 
-CONF_REMOTE_DEVICE_ID = "remote_device_id"
 CONF_MQTT_BASE_TOPIC = "mqtt_base_topic"
 CONF_REMOTE_FRIENDLY_NAME = "remote_friendly_name"
 CONF_TOPICS_TEXT = "topics_text"
@@ -26,10 +25,6 @@ button_5=kelvin:+300
 button_6=kelvin:-300
 button_7=next_effect
 button_8=all_off"""
-
-
-def device_selector():
-    return selector.DeviceSelector()
 
 
 def light_selector():
@@ -61,41 +56,39 @@ class RoomRemoteControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             self._abort_if_unique_id_configured()
             return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
-        return self.async_show_form(step_id="user", data_schema=vol.Schema({
+        schema = vol.Schema({
             vol.Required(CONF_NAME, default="Room remote"): str,
-            vol.Optional(CONF_REMOTE_DEVICE_ID): device_selector(),
             vol.Required(CONF_MQTT_BASE_TOPIC, default="zigbee2mqtt"): str,
-            vol.Optional(CONF_REMOTE_FRIENDLY_NAME, default=""): str,
+            vol.Required(CONF_REMOTE_FRIENDLY_NAME, default="remote_name"): str,
             vol.Optional(CONF_TOPICS_TEXT, default=""): text_selector(),
             vol.Required(CONF_LIGHTS, default=[]): light_selector(),
             vol.Optional(CONF_EXTRA_OFF, default=[]): light_selector(),
             vol.Required(CONF_BUTTONS_TEXT, default=DEFAULT_BUTTONS): text_selector(),
-        }), errors={})
+        })
+        return self.async_show_form(step_id="user", data_schema=schema, errors={})
 
     @staticmethod
     def async_get_options_flow(config_entry):
-        return RoomRemoteControlOptionsFlow()
+        return RoomRemoteControlOptionsFlow(config_entry)
 
 
 class RoomRemoteControlOptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self._entry = config_entry
+
     async def async_step_init(self, user_input: dict[str, Any] | None = None):
-        data = {**self.config_entry.data, **self.config_entry.options}
+        data = {**self._entry.data, **self._entry.options}
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        actions = discovered_actions_for_entry(self.hass, self.config_entry.entry_id)
+        actions = discovered_actions_for_entry(self.hass, self._entry.entry_id)
         default_rules = rules_template(actions, data.get(CONF_BUTTONS_TEXT, DEFAULT_BUTTONS))
-        fields = {
+        schema = vol.Schema({
             vol.Required(CONF_MQTT_BASE_TOPIC, default=data.get(CONF_MQTT_BASE_TOPIC, "zigbee2mqtt")): str,
-            vol.Optional(CONF_REMOTE_FRIENDLY_NAME, default=data.get(CONF_REMOTE_FRIENDLY_NAME, "")): str,
+            vol.Required(CONF_REMOTE_FRIENDLY_NAME, default=data.get(CONF_REMOTE_FRIENDLY_NAME, "")): str,
             vol.Optional(CONF_TOPICS_TEXT, default=data.get(CONF_TOPICS_TEXT, "")): text_selector(),
             vol.Required(CONF_LIGHTS, default=data.get(CONF_LIGHTS, [])): light_selector(),
             vol.Optional(CONF_EXTRA_OFF, default=data.get(CONF_EXTRA_OFF, [])): light_selector(),
             vol.Required(CONF_BUTTONS_TEXT, default=default_rules): text_selector(),
-        }
-        current_device = data.get(CONF_REMOTE_DEVICE_ID)
-        if current_device:
-            fields[vol.Optional(CONF_REMOTE_DEVICE_ID, default=current_device)] = device_selector()
-        else:
-            fields[vol.Optional(CONF_REMOTE_DEVICE_ID)] = device_selector()
-        return self.async_show_form(step_id="init", data_schema=vol.Schema(fields), errors={})
+        })
+        return self.async_show_form(step_id="init", data_schema=schema, errors={})
